0b4495b2586a44b0f3465953fc29876335f238b5
Awais Saleem <owaissaleem163@gmail.com>
2024-07-16 04:35:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+ReactDOM.render(<p>I am a react</p>,document.getElementById("root"));
+ReactDOM.render(<div><h1>React Course</h1>
+                <p>We need to learn react course in this video</p>,
+                </div>,document.getElementById("root"));
+  */             
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <div><h1>React Course</h1>
+                <p>We need to learn react course in this video</p>,
+                </div>
+);               
+/*
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <div>
+        <h1>This is heading </h1>
+        <P>this is a paragraph</P>,
+    </div>
+);
+*/
